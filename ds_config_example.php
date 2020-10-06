73518cd181fb81fa59f96a547cf65de56882dd03
Nathan Dench <ndenc2@gmail.com>
2020-10-06 17:26:53 +1000

    fix: Add vhost.conf

--- ds_config_example.php
+++ ds_config_example.php
@@ -8,7 +8,7 @@ $DS_CONFIG = [
     'ds_client_secret' => '{SECRET_KEY}', // The app's DocuSign integration key's secret
     'signer_email' => '{SIGNER_EMAIL}',
     'signer_name' => '{SIGNER_NAME}',
-    'app_url' => 'http://localhost:8080/public', // The url of the application.
+    'app_url' => 'http://localhost:8080', // The url of the application.
     // Ie, the user enters  app_url in their browser to bring up the app's home page
     // Eg http://localhost/code-examples-php/public if the app is installed in a
     // development directory that is accessible via web server.
