315dbf2821a4a54253508e7875940443fa9d24c3
Anna Hileta <80112359+annahileta@users.noreply.github.com>
2022-04-20 22:17:07 +0300

    Added responsive signing example (#65)
    * Added responsive signing example
    
    * Added try/catch block
    
    * Changed variables to CamelCase namings
    
    * fix email input value bug
    
    Co-authored-by: Aaron JacksonWilde <Aaron.Wilde@docusign.com>

---
+++ src/Controllers/Examples/eSignature/EG038ResponsiveSigning.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * Example 038: Responsive signing.
+*/
+
+namespace Example\Controllers\Examples\eSignature;
+
+use Example\Controllers\eSignBaseController;
+use Example\Services\Examples\eSignature\ResponsiveSigningService;
+use DocuSign\eSign\Client\ApiException;
+
+class EG038ResponsiveSigning extends eSignBaseController
+{
+    const EG = 'eg038'; # reference (and URL) for this example
+    const FILE = __FILE__;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        parent::controller();
+    }
+
+    /**
+     * 1. Check the token
+     * 2. Call the worker method
+     * 3. Redirect the user to the signing
+     *
+     * @return void
+     */
+    public function createController(): void
+    {
+        $this->checkDsToken();
+
+        try {
+            $returnUrl = ResponsiveSigningService::worker(
+                $this->args,
+                $this->clientService,
+                self::DEMO_DOCS_PATH
+            );
+
+            if ($returnUrl) {
+                header('Location: ' . $returnUrl);
+                exit;
+            }
+        } catch (ApiException $e) {
+            $this->clientService->showErrorTemplate($e);
+        }
+    }
+
+    /**
+     * Get specific template arguments
+     * @return array
+     */
+    public function getTemplateArgs(): array
+    {
+        $envelopeArgs = [
+            'signer_email' => $this->checkEmailInputValue($_POST['signer_email']),
+            'signer_name' => $this->checkInputValues($_POST['signer_name']),
+            'cc_email' => $this->checkEmailInputValue($_POST['cc_email']),
+            'cc_name' => $this->checkInputValues($_POST['cc_name']),
+            'signer_client_id' => $this->signer_client_id,
+            'ds_return_url' => $GLOBALS['app_url'] . 'index.php?page=ds_return'
+        ];
+        return [
+            'account_id' => $_SESSION['ds_account_id'],
+            'base_path' => $_SESSION['ds_base_path'],
+            'ds_access_token' => $_SESSION['ds_access_token'],
+            'envelope_args' => $envelopeArgs
+        ];
+    }
+}
---
+++ src/Services/Examples/eSignature/ResponsiveSigningService.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Example\Services\Examples\eSignature;
+
+use DocuSign\eSign\Client\ApiException;
+use DocuSign\eSign\Model\CarbonCopy;
+use DocuSign\eSign\Model\Document;
+use DocuSign\eSign\Model\EnvelopeDefinition;
+use DocuSign\eSign\Model\DocumentHtmlDefinition;
+use DocuSign\eSign\Model\Recipients;
+use DocuSign\eSign\Model\Signer;
+use Example\Services\SignatureClientService;
+
+class ResponsiveSigningService
+{
+    public static function worker(array $args, SignatureClientService $clientService, string $demoPath): string
+    {
+        $envelopeDefinition = ResponsiveSigningService::make_envelope($args["envelope_args"], $demoPath);
+        $envelopeApi = $clientService->getEnvelopeApi();
+
+        try {
+            $envelopeSummary = $envelopeApi->createEnvelope($args['account_id'], $envelopeDefinition);
+        } catch (ApiException $e) {
+            $clientService->showErrorTemplate($e);
+            exit;
+        }
+        $envelopeId = $envelopeSummary->getEnvelopeId();
+
+        $authenticationMethod = 'None';
+        $recipientViewRequest = $clientService->getRecipientViewRequest(
+            $authenticationMethod,
+            $args["envelope_args"]
+        );
+
+        $viewUrl = $clientService->getRecipientView($args['account_id'], $envelopeId, $recipientViewRequest);
+
+        return $viewUrl['url'];
+    }
+
+    /**
+     *  Creates envelope definition
+     *  Parameters for the envelope: signer_email, signer_name, cc_name, cc_email, signer_client_id
+     * @param  $args array
+     * @return EnvelopeDefinition -- returns an envelope definition
+     */
+    private static function make_envelope(array $args, string $demoPath): EnvelopeDefinition
+    {
+        $signer = new Signer(
+            [
+                'email' => $args['signer_email'],
+                'name' => $args['signer_name'],
+                'recipient_id' => "1",
+                'routing_order' => "1",
+                'client_user_id' => $args['signer_client_id'],
+                'role_name' => 'Signer'
+            ]
+        );
+
+        $cc = new CarbonCopy(
+            [
+                'email' => $args['cc_email'],
+                'name' => $args['cc_name'],
+                'recipient_id' => "2",
+                'routing_order' => "1"
+            ]
+        );
+
+        $htmlMarkupFileName = 'order_form.html';
+        $htmlMarkup = file_get_contents($demoPath . $htmlMarkupFileName);
+
+        $htmlWithData = str_replace(
+            ['{signer_name}', '{signer_email}', '{cc_name}', '{cc_email}', '/sn1/', '/l1q/', '/l2q/'],
+            [$args['signer_name'], $args['signer_email'], $args['cc_name'], $args['cc_email'], 
+            '<ds-signature data-ds-role="Signer"/>', ' <input data-ds-type="number"/>', '<input data-ds-type="number"/>'],
+            $htmlMarkup
+        );
+
+        return new EnvelopeDefinition(
+            [
+                'email_subject' => "Example Signing Document",
+                'documents' => [
+                    new Document(
+                        [
+                            'name' => 'Lorem Ipsum', # can be different from actual file name
+                            'document_id' => 1, # a label used to reference the doc,
+                            'html_definition' => new DocumentHtmlDefinition(
+                                [
+                                    'source' => $htmlWithData
+                                ]
+                            )
+                        ]
+                    )
+                ],
+                'recipients' => new Recipients(['signers' => [$signer], 'carbon_copies' => [$cc]]),
+                'status' => "sent" # requests that the envelope be created and sent.
+            ]
+        );
+    }
+}
