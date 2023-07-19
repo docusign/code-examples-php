e42dcae244335caa9b1c6ca3ddd26d6747737d5d
annahileta <anna.hileta@sigma.software>
2023-07-19 15:49:25 +0300

    fixed redirect for consent_required and added catch in case cfr account is not active

--- src/Services/utils.php
+++ src/Services/utils.php
@@ -4,6 +4,7 @@ namespace Example\Services;
 
 use DocuSign\eSign\Api\AccountsApi;
 use DocuSign\eSign\Client\ApiClient;
+use DocuSign\eSign\Client\ApiException;
 use DocuSign\eSign\Configuration;
 
 
@@ -16,13 +17,19 @@ class utils
      */
     public function isCFR($access_token, $account_id, $base_path)
     {
-        $config = new Configuration();
-        $config->setHost($base_path);
-        $config->addDefaultHeader('Authorization', 'Bearer ' . $access_token);
-        $this->api_client = new ApiClient($config);
-        $accountsApi = new AccountsApi($this->api_client);
-        $accountDetails = $accountsApi->getAccountInformation($account_id);
-        return $accountDetails["status21_cfr_part11"];
+        try {
+            $config = new Configuration();
+            $config->setHost($base_path);
+            $config->addDefaultHeader('Authorization', 'Bearer ' . $access_token);
+            $this->api_client = new ApiClient($config);
+
+            $accountsApi = new AccountsApi($this->api_client);
+            $accountDetails = $accountsApi->getAccountInformation($account_id);
+            var_dump($accountDetails["status21_cfr_part11"]);
+            return $accountDetails["status21_cfr_part11"];
+        } catch(ApiException $ex) {
+            return null;
+        }
     }
 
 
