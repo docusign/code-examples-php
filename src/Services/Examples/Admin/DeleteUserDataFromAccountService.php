cb37838be4c8760c46dc52d433d9fc2d2f6b9835
Paige Rossi <paige.rossi@docusign.com>
2023-07-27 11:51:58 -0700

    fixing codeDepot snippets for admin 11

--- src/Services/Examples/Admin/DeleteUserDataFromAccountService.php
+++ src/Services/Examples/Admin/DeleteUserDataFromAccountService.php
@@ -23,13 +23,13 @@ class DeleteUserDataFromAccountService
         string $accountId,
         string $userId
     ): IndividualUserDataRedactionResponse {
-        #ds-snippet-start:Admin10Step3
+        #ds-snippet-start:Admin11Step3
         $membershipDataRedaction = new IndividualMembershipDataRedactionRequest();
         $membershipDataRedaction->setUserId($userId);
-        #ds-snippet-end:Admin10Step3
+        #ds-snippet-end:Admin11Step3
 
-        #ds-snippet-start:Admin10Step4
+        #ds-snippet-start:Admin11Step4
         return $accountsApi->redactIndividualMembershipData($accountId, $membershipDataRedaction);
-        #ds-snippet-end:Admin10Step4
+        #ds-snippet-end:Admin11Step4
     }
 }
